8cf697491aa60cf678f4353b691f1501b016fb38
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2022-08-27 04:12:00 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 // import { async } from "@firebase/util";
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+// import { Link, useNavigate } from "react-router-dom";
 // import { UserAuth } from "../AuthContext";
 
 const Navbar = () => {
