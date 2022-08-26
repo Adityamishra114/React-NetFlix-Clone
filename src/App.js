6d72556c41f4eba75f973affd4e9ffe2a39fbaef
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2022-08-27 01:01:28 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import { AuthContextProvider } from "./AuthContext";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import Account from "./pages/Account";
-import ProtectedRoute from "./components/Protected";
+// import { AuthContextProvider } from "./AuthContext";
+// import Login from "./pages/Login";
+// import SignUp from "./pages/SignUp";
+// import Account from "./pages/Account";
+// import ProtectedRoute from "./components/Protected";
 function App() {
   return (
     <>
-      <AuthContextProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+      {/* <AuthContextProvider> */}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route
             path="/account"
@@ -23,9 +23,9 @@ function App() {
                 <Account />
               </ProtectedRoute>
             }
-          />
-        </Routes>
-      </AuthContextProvider>
+          /> */}
+      </Routes>
+      {/* </AuthContextProvider> */}
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,24 @@
-import { async } from "@firebase/util";
+// import { async } from "@firebase/util";
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { UserAuth } from "../AuthContext";
+// import { UserAuth } from "../AuthContext";
 
 const Navbar = () => {
-  const { user, logOut } = UserAuth();
-  const navigate = useNavigate();
+  // const { user, logOut } = UserAuth();
+  // const navigate = useNavigate();
 
-  const handleLogout = async () => {
-    try {
-      await logOut();
-      navigate("/");
-    } catch (error) {}
-  };
+  // const handleLogout = async () => {
+  //   try {
+  //     await logOut();
+  //     navigate("/");
+  //   } catch (error) {}
+  // };
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
-      <Link to="/">
-        <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
-          NETFLIX
-        </h1>
-      </Link>
+      <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+        NETFLIX
+      </h1>
+      {/* </Link>
       {user?.email ? (
         <div>
           <Link to="/account">
@@ -27,24 +26,30 @@ const Navbar = () => {
           </Link>
 
           <button
-            onClick={handleLogout}
+            // onClick={handleLogout}
             className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white"
           >
             Logout
           </button>
         </div>
       ) : (
-        <div>
-          <Link to="/login">
+        <div> */}
+      {/* <Link to="/login">
             <button className="text-white pr-4">Sign In</button>{" "}
           </Link>
           <Link to="/signup">
             <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
               Sign Out
             </button>
-          </Link>
-        </div>
-      )}
+          </Link> */}
+      {/* </div>
+      )} */}
+      <div>
+        <button className="text-white pr-4">Sign In</button>{" "}
+        <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+          Sign Out
+        </button>
+      </div>
     </div>
   );
 };
