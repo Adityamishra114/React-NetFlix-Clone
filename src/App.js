e5fbe6e879713f3dfa6f1b51cbbd52cfadece6ed
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2022-08-27 04:34:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,30 +2,12 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-// import { AuthContextProvider } from "./AuthContext";
-// import Login from "./pages/Login";
-// import SignUp from "./pages/SignUp";
-// import Account from "./pages/Account";
-// import ProtectedRoute from "./components/Protected";
 function App() {
   return (
     <>
-      {/* <AuthContextProvider> */}
       <Navbar />
       <Routes>
         <Route exact path={process.env.PUBLIC_URL + "/"} element={<Home />} />
-        {/* <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route
-            path="/account"
-            element={
-              <ProtectedRoute>
-                <Account />
-              </ProtectedRoute>
-            }
-          /> */}
-      </Routes>
-      {/* </AuthContextProvider> */}
     </>
   );
 }
